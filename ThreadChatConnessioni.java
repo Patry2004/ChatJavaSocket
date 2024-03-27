bcff1d823ed5223c087af8f048d99bca1a3c111c
Patry2004 <156059796+Patry2004@users.noreply.github.com>
2024-03-27 08:33:23 +0100

    Update ThreadChatConnessioni.java

--- ThreadChatConnessioni.java
+++ ThreadChatConnessioni.java
@@ -1,7 +1,7 @@
 import java.net.*;
 import java.io.*;
 
-public class ThreadChatConnessioni implements Runnable {
+public class ThreadChatConnessioni extends Thread {
     private ThreadGestioneServizioChat gestoreChat;
     private Socket client = null;
     private Bufferedreader input = null;
@@ -16,33 +16,29 @@ public class ThreadChatConnessioni implements Runnable {
             this.input = new BufferedReader(new InputStreamReader(client.getInputStream()));
             this.output = new PintWriter(this.client.getOutputStream(), true);
         } catch (Exception e) {
-            output.println("Errore nella lettura");
+            System.out.println("Errore nella lettura");
         }
         me = new Thread(this);
         me.start();
     }
 
     public void run() {
-        while (true) {
             try {
+
                 String mex = null;
                 // rimango in attesa del messaggio mandato dal client
-                while ((mex = input.readLine()) == null) {
+                while ((mex = input.readLine()) != null) {
+					gestoreChat.spedisciMessaggio(mex);
                 }
                 // invoco il metodo del gestoreChat per ripetere a tutti il messaggio ricevuto
-                gestoreChat.spedisciMessaggio(mex);
-            } catch (Exception e) {
-                output.println("Errore nella spediione del messaggio a tutti.");
+            } catch (IOException e) {
+                System.out.println("Errore durante la chiusura della connessione: " + e.getMessage());
             }
         }
     }
 
     public void spedisciMessaggioChat(String messaggio) {
-        try {
             output.println(messaggio);
-
-        } catch (Exception e) {
-            output.println("Errore nella spedizione del singolo messaggio.");
-        }
+    
     }
 }
